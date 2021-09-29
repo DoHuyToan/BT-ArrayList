9622abe66b4848dfb85334699e05d4966b8073d4
DoHuyToan <dhtoan89@gmail.com>
2021-09-29 20:54:46 +0700

    ok

---
+++ src/MyListTest.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> integerList = new MyList<>();
+        integerList.add(0,1);
+        integerList.add(1,2);
+        integerList.add(2,3);
+        integerList.add(3,4);
+        integerList.add(4,5);
+
+
+
+        System.out.println(integerList.get(0));
+        System.out.println(integerList.get(1));
+    }
+}
